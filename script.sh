7cc9ad38fcfd3909cc1562efefce327f2c69f396
Patrick Presley <57566465+presleyp84@users.noreply.github.com>
2022-11-13 17:58:18 +0530

    updated script to input dd api key as argument

--- script.sh
+++ script.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 while true
  do
-   DD_SITE="datadoghq.com" DD_API_KEY=$DD_API_KEY python3 "custom_metric.py"
+   DD_SITE="datadoghq.com" DD_API_KEY=$1 python3 "custom_metric.py"
    sleep 60;
  done
