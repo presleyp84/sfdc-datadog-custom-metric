f9b01f6476a6170b234962b70b0616d4c42a2018
Patrick Presley <57566465+presleyp84@users.noreply.github.com>
2022-11-13 17:54:12 +0530

    updated script for dockerfile

--- script.sh
+++ script.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
-DD_SITE="datadoghq.com" DD_API_KEY=$DD_API_KEY python3 "sample_dev.py"
-
- while true
+while true
  do
    DD_SITE="datadoghq.com" DD_API_KEY=$DD_API_KEY python3 "custom_metric.py"
    sleep 60;
